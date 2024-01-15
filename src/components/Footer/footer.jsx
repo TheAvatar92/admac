16b1b5eeac99456c6170ad9c714f6a07aa91376d
Chris <mccomb.cc@gmail.com>
2024-01-15 22:56:08 +0000

    resize1

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -4,8 +4,10 @@ import './footer.css'
 const Footer = () => {
   return (
     <footer className="footer">
+      <div className="footText">
         <p>Copyright &#169; 2024 ADMAC. All Rights Reserved.</p>
         <p className='design'>Website designed and built by CC Developers</p>
+        </div>
     </footer>
   )
 }
